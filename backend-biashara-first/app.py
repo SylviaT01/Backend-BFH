51e6ab2f094784cd259c123dd851dfe0c3ba37ef
Sylvia Chebet <sylviachebet03@gmail.com>
2024-10-16 07:34:47 +0300

    Modify database path

--- backend-biashara-first/app.py
+++ backend-biashara-first/app.py
@@ -8,7 +8,8 @@ import os
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital_locator.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital_locator.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///hospital_locator.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
